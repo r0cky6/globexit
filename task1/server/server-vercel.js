aecffa34fd499bfac971a8fb08559ce697c6543e
Vyacheslav Panfilov <vyacheslav.lmi@gmail.com>
2023-10-23 04:22:20 +0600

    adapt server for deployment on versel

---
+++ task1/server/server-vercel.js
@@ -0,0 +1,25 @@
+import { readFile } from 'fs';
+import { resolve } from 'path';
+
+export default function appPlugin(fastify, _, done) {
+	fastify.register(require('fastify-cors'));
+
+	fastify.get('/', async (request, reply) => {
+		readFile(resolve(__dirname, 'users.json'), 'utf8', (err, data) => {
+			if (err) {
+				console.log('File read failed:', err);
+				reply.send('err: ' + err.toString())
+				return;
+			}
+			if (request.query.term) {
+				const result = JSON.parse(data).filter((elem) => elem.name.toLowerCase().search(request.query.term.toLowerCase()) !== -1);
+				reply.send(JSON.stringify(result));
+			}
+			else {
+				reply.send(data);
+			}
+		})
+	})
+
+	done()
+}
